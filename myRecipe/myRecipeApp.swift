9940b3356b9abf831bfbf58722c0d4ef05085e4a
thuyiswater <s3877746@rmit.edu.vn>
2023-09-21 12:29:15 +0700

    Initial Commit

---
+++ myRecipe/myRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  myRecipeApp.swift
+//  myRecipe
+//
+//  Created by mai chieu thuy on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct myRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
